37130fd131f2c8b765dcfbea336bf33e368b07fd
Priyadharshini <priyasaravanan282@gmail.com>
2024-09-24 22:36:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+ 
+  return (
+    <>
+    <div >
+      <h1 className='heading'>Avangers here !!</h1>
+    
+      </div> 
+     
+     <div className='gallery'>
+      <div className='image image1'></div>
+      <div className='image image2'></div>
+      <div className='image image3'></div>
+      <div className='image image4'></div>
+     
+      <div className='image image6'></div>
+     </div>
+        
+    </>
+  )
+}
+
+export default App
